b4549fe8f47c5357a447f317db0f8500f45bc94c
Dinh263 <dinh.test263@hotmail.com>
2018-02-19 01:14:41 -0800

    first commit

---
+++ AutomationFramework/AutomationFramework/SeleniumUtils.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationFramework
+{
+    public class SeleniumUtils
+    {
+        public static string TakeScreenshot(IWebDriver driver)
+        {
+            // Get the current TimeStamp
+            string timeStamp = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss-fff");
+            // Build a file name (*NOTE: The folder must already exist and allow writes to it)
+            string fileName = @"C:\temp\Selenium\" + timeStamp + ".jpg";
+            // Take a screenshot
+            Screenshot screenshot = ((ITakesScreenshot)driver).GetScreenshot();
+            screenshot.SaveAsFile(fileName, ScreenshotImageFormat.Jpeg);
+            // Return the filename of the screenshot
+            return fileName;
+        }
+
+    }
+}
---
+++ AutomationFramework/AutomationFramework/TestBase.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+namespace AutomationFramework
+{
+    public class TestBase
+    {
+        protected IWebDriver driver;
+        protected string baseUrl = ConfigurationManager.AppSettings["baseUrl"];
+        public void SetUp(string browserName)
+        {
+            if (browserName.ToLower().Equals("ie"))
+            {
+                driver = new InternetExplorerDriver();
+            }
+            else if (browserName.ToLower().Equals("chrome"))
+            {
+                driver = new ChromeDriver();
+            }
+            else
+            {
+                driver = new FirefoxDriver();
+            }
+            driver.Manage().Window.Maximize();
+            driver.Manage().Cookies.DeleteAllCookies();
+ 
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Quit();
+        }
+
+        public static IEnumerable<string> GetBrowsers()
+        {
+            string configBrowser = ConfigurationManager.AppSettings["browsers"] ?? "chrome";
+            string[] browsers = configBrowser.Split(',');
+            foreach(string browser in browsers)
+            {
+                yield return browser;
+            }
+        }
+    }
+}
